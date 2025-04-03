5a62ccbf956488a62d5228a9ebd9c0c435e0a70b
Rens <mulderrens@outlook.com>
2025-04-03 16:01:28 +0200

    Add item object

---
+++ Objects/Items/Item.cs
@@ -0,0 +1,30 @@
+﻿using HealthyBusiness.Engine;
+using HealthyBusiness.Engine.Utils;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HealthyBusiness.Objects.Items
+{
+    public class Item : TiledGameObject
+    {
+        private string _textureName;
+        private Texture2D _texture;
+        public Item(TileLocation tileLocation, string textureName) : base(tileLocation)
+        {
+            _textureName = textureName;
+        }
+
+        public override void Load(ContentManager content)
+        {
+            base.Load(content);
+            _texture = content.Load<Texture2D>(_textureName);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            spriteBatch.Draw(_texture, new Rectangle(new Point(TileLocation.X, TileLocation.Y), new Point(Globals.TILESIZE, Globals.TILESIZE)), Color.White);
+        }
+    }
+}
