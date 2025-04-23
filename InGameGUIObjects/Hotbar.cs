b6f52bc6bbb9e01a9c2b2f9034d5f09913e57162
Jurian-24 <jurian.andriessen@gmail.com>
2025-04-23 15:27:41 +0200

    chore(feat): added the base for the hotbar class

---
+++ InGameGUIObjects/Hotbar.cs
@@ -0,0 +1,29 @@
+﻿using HealthyBusiness.Objects.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyBusiness.InGameGUIObjects
+{
+    class Hotbar
+    {
+        List<Item> hotbarItems;
+
+        public Hotbar()
+        {
+            this.hotbarItems = new List<Item>();
+        }
+
+        public void Add(Item item) => hotbarItems.Add(item);
+
+        public void Remove(Item item)
+        {
+            if (hotbarItems.Contains(item))
+            {
+                hotbarItems.Remove(item);
+            }
+        }
+    }
+}
