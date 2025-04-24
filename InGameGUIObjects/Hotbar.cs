37953e3d26420ed1f04e96e3e2d68e0536df61b5
Rens <mulderrens@outlook.com>
2025-04-24 13:02:00 +0200

    Small fixes

--- InGameGUIObjects/Hotbar.cs
+++ InGameGUIObjects/Hotbar.cs
@@ -13,13 +13,10 @@ namespace HealthyBusiness.InGameGUIObjects
     {
         private const int AMOUNT_OF_SLOTS = Globals.HOTBAR_SLOTS;
 
-        private List<HotbarSlot> createdHotbarSlots = new List<HotbarSlot>();
-
-        public Item[] hotbarItems;
+        public List<HotbarSlot> HotbarSlots = new List<HotbarSlot>();
 
         public Hotbar()
         {
-            this.hotbarItems = new Item[AMOUNT_OF_SLOTS];
             InitializeHotbarSlots();
         }
 
@@ -27,9 +24,9 @@ namespace HealthyBusiness.InGameGUIObjects
         {
             for (int i = 0; i < AMOUNT_OF_SLOTS; i++)
             {
-                HotbarSlot hotbarSlot = new HotbarSlot(hotbarItems[i] ?? null);
+                HotbarSlot hotbarSlot = new HotbarSlot();
                 hotbarSlot.Load(GameManager.GetGameManager().ContentManager);
-                createdHotbarSlots.Add(hotbarSlot);
+                HotbarSlots.Add(hotbarSlot);
             }
         }
 
@@ -37,32 +34,15 @@ namespace HealthyBusiness.InGameGUIObjects
         {
             for (int i = 0; i < AMOUNT_OF_SLOTS; i++)
             {
-                if (hotbarItems[i] == null)
+                var slot = HotbarSlots[i];
+                if (slot.Item == null)
                 {
-                    hotbarItems[i] = item;
+                    slot.Item = item;
                     return true;
                 }
             }
 
             return false; // no empty slot 
-        }
-
-        public bool Remove(Item item)
-        {
-            for (int i = 0; i < AMOUNT_OF_SLOTS; i++)
-            {
-                if (hotbarItems[i] == item)
-                {
-                    hotbarItems[i] = null;
-                    return true;
-                }
-            }
-            return false; // item not found
-        }
-
-        public void Clear()
-        {
-            //_hotbarItems.Clear();
         }
 
         public override void Load(ContentManager content)
@@ -75,23 +55,29 @@ namespace HealthyBusiness.InGameGUIObjects
             base.Update(gameTime);
         }
 
+        public override void Unload()
+        {
+            base.Unload();
+            HotbarSlots.Clear();
+        }
+
         public override void Draw(SpriteBatch spriteBatch)
         {
             // draw the hotbar slots
-            for (int i = 0; i < createdHotbarSlots.Count; i++)
+            for (int i = 0; i < HotbarSlots.Count; i++)
             {
-                HotbarSlot slot = createdHotbarSlots[i];
+                HotbarSlot slot = HotbarSlots[i];
 
                 slot.Draw(
                     spriteBatch,
                     (i > 0)                             // check if te iteration is the first index
-                        ? createdHotbarSlots[i - 1]     // if it is not the first iteration, pass the previous slot
+                        ? HotbarSlots[i - 1]     // if it is not the first iteration, pass the previous slot
                         : null                         // if it is the first iteration, pass null to prevent a index out of range exception
                                                        //hotbarItems[i] // ready for when the player can add to the hotbar from the itempickup module       
                 );
             }
 
-            //System.Diagnostics.Debug.WriteLine(createdHotbarSlots.Count);
+            //System.Diagnostics.Debug.WriteLine(HotbarSlots.Count);
 
             // draw the hotbar container // TODO: niet nodig waarschijnlijk
             //Texture2D hotbarcontainer = new Texture2D(spriteBatch.GraphicsDevice, 100, 100);
