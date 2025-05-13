d32c52bc5b9a4071ba7d46ab3ff2ef3bbebf0536
Kim <1039564@hr.nl>
2025-05-13 14:32:16 +0200

    Added 'Paused' text

--- InGameGUIObjects/PauseMenu.cs
+++ InGameGUIObjects/PauseMenu.cs
@@ -25,7 +25,7 @@ namespace HealthyBusiness.InGameGUIObjects
         public override void Load(ContentManager content)
         {
             Add(new ColoredBackground(Color.Black * 0.5f));
-            Add(new Text("fonts\\pixelated_elegance\\title", "Healthy Business", Color.White, new GUIStyling(marginTop: 200, horizontalFloat: HorizontalAlign.Center)));
+            Add(new Text("fonts\\pixelated_elegance\\title", "Paused", Color.White, new GUIStyling(marginTop: 200, horizontalFloat: HorizontalAlign.Center)));
            
             var resumeButton = new TextedButton("Resume", new GUIStyling(marginTop: 300, horizontalFloat: HorizontalAlign.Center));
             resumeButton.Clicked += ResumeButtonClicked;
