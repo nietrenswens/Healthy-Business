6761844666793d7f5f68a9cb7a226ef7d2030820
Kim <1039564@hr.nl>
2025-05-21 12:28:18 +0200

    Player animatie speelt op basis van meegegeven row (hardcoded)

---
+++ Objects/Creatures/Player/Player.cs
@@ -0,0 +1,66 @@
+﻿using HealthyBusiness.Collision;
+using HealthyBusiness.Controllers;
+using HealthyBusiness.Engine;
+using HealthyBusiness.Engine.Utils;
+using HealthyBusiness.Objects.Creatures.Player.Modules;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HealthyBusiness.Objects.Creatures.Player
+{
+    public class Player : Creature
+    {
+
+        protected Animation _animation;
+
+        public Player(Vector2 spawnPosition) : base(spawnPosition, 100, 100)
+        {
+            LocalScale = 4;
+            Add(new CollidableMovementController(CollisionGroup.Solid));
+            Add(new PlayerInputController());
+            Add(new ItemPickupModule());
+
+        }
+
+        public Player(TileLocation tileLocation) : this(tileLocation.ToVector2())
+        { }
+
+        public override void Load(ContentManager content)
+        {
+            Texture = content.Load<Texture2D>("entities\\player");
+            var width = (int)(Texture.Width * LocalScale);
+            var height = (int)(Texture.Height * LocalScale);
+
+            var collider = new RectangleCollider(new Rectangle(WorldPosition.ToPoint(), new Point(width, height / 2)));
+            collider.LocalPosition = new Vector2(0, height / 2);
+            collider.CollisionGroup = CollisionGroup.Player;
+            Add(collider);
+            base.Load(content);
+
+            _animation = new PlayerAnimation("entities\\daniel", 3);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (_animation.CurrentRow != 3)
+            {
+                _animation.SetRow(3);
+            }
+            _animation.Update(gameTime);
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+            base.OnCollision(other);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            _animation.Draw(spriteBatch, WorldPosition, LocalScale/2);
+            //base.Draw(spriteBatch);
+        }
+
+    }
+}
